698c364e6ce9de1f0d118e323e4b3f42bc9a7311
Paola Jocelin Septimo Velazquez <Paola Jocelin Septimo Velazquez>
2017-05-09 17:13:45 -0500

    Agregando funcionalidad al tache

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,11 @@
-var cerrarPandas =document.getElementsByClassName(“tache”);
+var cerrarPandas=document.getElementsByClassName("tache");
+var fotoPanda=document.getElementsByClassName("fotoPanda");
+
+for(var i=0; i<cerrarPandas.length; i++){
+  cerrarPandas[i].addEventListener("click",quitarImagen);
+}
+
+function quitarImagen() {
+  var div = this.parentElement;
+  div.style.display = "none";
+}
